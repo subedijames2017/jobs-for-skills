d325f1a7a758a1d12ce89942f1c503c2416a18ed
jyotindra subedi <jyotindra@proshore.eu>
2020-05-17 15:39:14 +0545

    added router

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,20 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import { Tab, Tabs, Row, Nav, Col } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
 import Landingpage from "./components/Landingpage";
 import About from "./components/About";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App wrapper">
-      <Navbar />
-      <Landingpage />
-    </div>
+    <Router>
+      <div className="App wrapper">
+        <Navbar />
+        <Route exact path="/" component={Landingpage} />
+        <Route exact path="/about" component={About} />
+      </div>
+    </Router>
   );
 }
 
--- client/src/components/About.js
+++ client/src/components/About.js
@@ -2,6 +2,6 @@ import React, { Component } from "react";
 
 export default class About extends Component {
   render() {
-    return <div>this is about page for more information</div>;
+    return <div id="content">this is about page for more information</div>;
   }
 }
